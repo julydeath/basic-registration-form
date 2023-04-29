b22d423b84939832686cc06465520c537262bc93
julydeath <manojkarajada.mk@gmail.com>
2023-04-29 14:36:53 +0530

    init : basic react form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form className="register-form">
+        <input placeholder="First Name" type="text" className="form-field" />
+        <input placeholder="Last Name" type="text" className="form-field" />
+        <input placeholder="E-mail" type="email" className="form-field" />
+        <button>Register</button>
+      </form>
     </div>
   );
 }
